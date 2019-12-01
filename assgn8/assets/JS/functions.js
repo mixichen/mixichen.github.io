e8143d58221860c6f0acc1ceb8ecf2405dea1b8a
Missy Chen <yingxuc@andrew.cmu.edu>
2019-11-30 21:38:13 -0500

    Upload Assignment 8

---
+++ assgn8/assets/JS/functions.js
@@ -0,0 +1,249 @@
+
+
+$( document ).ready(function() {
+
+  var canScroll = true,
+      scrollController = null;
+  $(this).on('mousewheel DOMMouseScroll', function(e){
+
+    if (!($('.outer-nav').hasClass('is-vis'))) {
+
+      var delta = (e.originalEvent.wheelDelta) ? -e.originalEvent.wheelDelta : e.originalEvent.detail * 20;
+
+      if (delta > 50 && canScroll) {
+        canScroll = false;
+        clearTimeout(scrollController);
+        scrollController = setTimeout(function(){
+          canScroll = true;
+        }, 800);
+        updateHelper(1);
+      }
+      else if (delta < -50 && canScroll) {
+        canScroll = false;
+        clearTimeout(scrollController);
+        scrollController = setTimeout(function(){
+          canScroll = true;
+        }, 800);
+        updateHelper(-1);
+      }
+
+    }
+
+  });
+
+  $('.side-nav li, .outer-nav li').click(function(){
+
+    if (!($(this).hasClass('is-active'))) {
+
+      var $this = $(this),
+          curActive = $this.parent().find('.is-active'),
+          curPos = $this.parent().children().index(curActive),
+          nextPos = $this.parent().children().index($this),
+          lastItem = $(this).parent().children().length - 1;
+
+      updateNavs(nextPos);
+      updateContent(curPos, nextPos, lastItem);
+
+    }
+
+  });
+
+  $('.cta').click(function(){
+
+    var curActive = $('.side-nav').find('.is-active'),
+        curPos = $('.side-nav').children().index(curActive),
+        lastItem = $('.side-nav').children().length - 1,
+        nextPos = lastItem;
+
+    updateNavs(lastItem);
+    updateContent(curPos, nextPos, lastItem);
+
+  });
+
+  
+
+  // determine scroll, swipe, and arrow key direction
+  function updateHelper(param) {
+
+    var curActive = $('.side-nav').find('.is-active'),
+        curPos = $('.side-nav').children().index(curActive),
+        lastItem = $('.side-nav').children().length - 1,
+        nextPos = 0;
+
+    if (param.type === "swipeup" || param.keyCode === 40 || param > 0) {
+      if (curPos !== lastItem) {
+        nextPos = curPos + 1;
+        updateNavs(nextPos);
+        updateContent(curPos, nextPos, lastItem);
+      }
+      else {
+        updateNavs(nextPos);
+        updateContent(curPos, nextPos, lastItem);
+      }
+    }
+    else if (param.type === "swipedown" || param.keyCode === 38 || param < 0){
+      if (curPos !== 0){
+        nextPos = curPos - 1;
+        updateNavs(nextPos);
+        updateContent(curPos, nextPos, lastItem);
+      }
+      else {
+        nextPos = lastItem;
+        updateNavs(nextPos);
+        updateContent(curPos, nextPos, lastItem);
+      }
+    }
+
+  }
+
+  // sync side and outer navigations
+  function updateNavs(nextPos) {
+
+    $('.side-nav, .outer-nav').children().removeClass('is-active');
+    $('.side-nav').children().eq(nextPos).addClass('is-active');
+    $('.outer-nav').children().eq(nextPos).addClass('is-active');
+
+  }
+
+  // update main content area
+  function updateContent(curPos, nextPos, lastItem) {
+
+    $('.main-content').children().removeClass('section--is-active');
+    $('.main-content').children().eq(nextPos).addClass('section--is-active');
+    $('.main-content .section').children().removeClass('section--next section--prev');
+
+    if (curPos === lastItem && nextPos === 0 || curPos === 0 && nextPos === lastItem) {
+      $('.main-content .section').children().removeClass('section--next section--prev');
+    }
+    else if (curPos < nextPos) {
+      $('.main-content').children().eq(curPos).children().addClass('section--next');
+    }
+    else {
+      $('.main-content').children().eq(curPos).children().addClass('section--prev');
+    }
+
+    if (nextPos !== 0 && nextPos !== lastItem) {
+      $('.header--cta').addClass('is-active');
+    }
+    else {
+      $('.header--cta').removeClass('is-active');
+    }
+
+  }
+
+
+
+  function workSlider() {
+
+  
+
+    $('.slider--prev, .slider--next').click(function() {
+
+      var $this = $(this),
+          curLeft = $('.slider').find('.slider--item-left'),
+          curLeftPos = $('.slider').children().index(curLeft),
+          curCenter = $('.slider').find('.slider--item-center'),
+          curCenterPos = $('.slider').children().index(curCenter),
+          curRight = $('.slider').find('.slider--item-right'),
+          curRightPos = $('.slider').children().index(curRight),
+          totalWorks = $('.slider').children().length,
+          $left = $('.slider--item-left'),
+          $center = $('.slider--item-center'),
+          $right = $('.slider--item-right'),
+          $item = $('.slider--item');
+
+      $('.slider').animate({ opacity : 0 }, 400);
+
+      setTimeout(function(){
+
+      if ($this.hasClass('slider--next')) {
+        if (curLeftPos < totalWorks - 1 && curCenterPos < totalWorks - 1 && curRightPos < totalWorks - 1) {
+          $left.removeClass('slider--item-left').next().addClass('slider--item-left');
+          $center.removeClass('slider--item-center').next().addClass('slider--item-center');
+          $right.removeClass('slider--item-right').next().addClass('slider--item-right');
+        }
+        else {
+          if (curLeftPos === totalWorks - 1) {
+            $item.removeClass('slider--item-left').first().addClass('slider--item-left');
+            $center.removeClass('slider--item-center').next().addClass('slider--item-center');
+            $right.removeClass('slider--item-right').next().addClass('slider--item-right');
+          }
+          else if (curCenterPos === totalWorks - 1) {
+            $left.removeClass('slider--item-left').next().addClass('slider--item-left');
+            $item.removeClass('slider--item-center').first().addClass('slider--item-center');
+            $right.removeClass('slider--item-right').next().addClass('slider--item-right');
+          }
+          else {
+            $left.removeClass('slider--item-left').next().addClass('slider--item-left');
+            $center.removeClass('slider--item-center').next().addClass('slider--item-center');
+            $item.removeClass('slider--item-right').first().addClass('slider--item-right');
+          }
+        }
+      }
+      else {
+        if (curLeftPos !== 0 && curCenterPos !== 0 && curRightPos !== 0) {
+          $left.removeClass('slider--item-left').prev().addClass('slider--item-left');
+          $center.removeClass('slider--item-center').prev().addClass('slider--item-center');
+          $right.removeClass('slider--item-right').prev().addClass('slider--item-right');
+        }
+        else {
+          if (curLeftPos === 0) {
+            $item.removeClass('slider--item-left').last().addClass('slider--item-left');
+            $center.removeClass('slider--item-center').prev().addClass('slider--item-center');
+            $right.removeClass('slider--item-right').prev().addClass('slider--item-right');
+          }
+          else if (curCenterPos === 0) {
+            $left.removeClass('slider--item-left').prev().addClass('slider--item-left');
+            $item.removeClass('slider--item-center').last().addClass('slider--item-center');
+            $right.removeClass('slider--item-right').prev().addClass('slider--item-right');
+          }
+          else {
+            $left.removeClass('slider--item-left').prev().addClass('slider--item-left');
+            $center.removeClass('slider--item-center').prev().addClass('slider--item-center');
+            $item.removeClass('slider--item-right').last().addClass('slider--item-right');
+          }
+        }
+      }
+
+    }, 400);
+
+    $('.slider').animate({ opacity : 1 }, 400);
+
+    });
+
+
+
+
+  }
+
+
+
+  function transitionLabels() {
+
+    $('input[name="type"]').on('click', function() {
+        if ($(this).val() == 'vape_pens') {
+            $('#text-1').show();
+            $('#text-2').hide();
+            $('#text-3').hide();
+
+
+        }
+        else if($(this).val() == 'tinctures'){
+            $('#text-1').hide();
+            $('#text-2').show();
+            $('#text-3').hide();
+        }
+        else if($(this).val() == 'edibles'){
+            $('#text-1').hide();
+            $('#text-2').hide();
+            $('#text-3').show();
+        }
+    });
+
+  }
+
+ 
+  workSlider();
+  transitionLabels();
+
+});
